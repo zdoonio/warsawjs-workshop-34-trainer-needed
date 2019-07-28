6b6def439c3ca51fb9876b04094521040c7fd5bc
zdoonio <dominik.zdunczyk@gmail.com>
2019-07-28 11:53:48 +0200

    implemented change communication protocol to web socket

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -2,6 +2,23 @@
 
     function main() {
         console.log('hello world');
+
+        const url = 'ws://localhost:3000';
+        const socket = new WebSocket(url);
+
+        socket.addEventListener('open', () => {
+            console.log('open');
+        });
+        socket.addEventListener('message', () => {
+            console.log('message');
+        });
+        socket.addEventListener('close', () => {
+            console.log('close');
+        });
+        socket.addEventListener('error', () => {
+            console.log('error');
+        });
+
     }
 
     root.addEventListener('DOMContentLoaded', main);
--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const ws = require('ws');
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({
+    path: path.join(__dirname, 'config', 'app.env')
+});
+
+const port = Number(process.env.PORT);
+
+const settings = { port };
+const server = new ws.Server(settings);
+
+server.on('headers', () => {
+   console.log('headers');
+});
+
+server.on('connection', () => {
+    console.log('connection');
+});
+
+server.on('listening', () => {
+    console.log('listening');
+});
+
+server.on('error', () => {
+    console.log('error');
+});
